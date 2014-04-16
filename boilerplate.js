561d819ae9ae0a888d5b1aa0bb1e5ae34cdf8a9b
Anderson Freitas <anderson.amf@gmail.com>
2014-04-16 17:17:33 -0300

    extraindo arquivo

--- boilerplate.js
+++ boilerplate.js
@@ -1,17 +1,3 @@
-/**
- * Provides requestAnimationFrame in a cross browser way.
- * paulirish.com/2011/requestanimationframe-for-smart-animating/
- */
-window.requestAnimationFrame = window.requestAnimationFrame || ( function() {
-  return  window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function(  callback, element ) {
-            window.setTimeout( callback, 1000 / 60 );
-          };
-})();
-
 var canvas;
 var gl;
 var buffer;
