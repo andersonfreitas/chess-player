c8e76fecbc997a8788286ba3062baffc16c31dce
Anderson Freitas <anderson.amf@gmail.com>
2014-04-27 15:43:37 -0300

    renderizando board flat

--- app/Board.js
+++ app/Board.js
@@ -1,54 +1,48 @@
 function Board() {
   BaseObject.call(this);
 
-  this.vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(_.flatten(this.getVertices())), gl.STATIC_DRAW);
-  this.vertexBuffer.itemSize = 2;
-  this.vertexBuffer.numItems = this.getVertices().length;
-
-  this.indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(_.flatten(this.getIndices())), gl.STATIC_DRAW);
-
-  var flagColors = [];
-  for (i = 0; i < this.getVertices().length; i++) flagColors.push(vec3.fromValues(0, 0, 0));
-
-  this.colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(_.flatten(flagColors)), gl.STATIC_DRAW);
-  this.colorBuffer.itemSize = 3;
-  this.colorBuffer.numItems = flagColors.length;
+  this.position = vec3.create();
+  this.createBoard();
+  this.initBuffers();
 }
 
 Board.prototype = new BaseObject();
 Board.prototype.constructor = Board;
 
-Board.prototype.render = function() {
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-  gl.vertexAttribPointer(currentProgram.vertexPositionAttribute, this.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+Board.prototype.createBoard = function() {
+  var color;
+  function cycleColors(a, b) {
+    color = (color !== a) ? a : b;
+  }
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
-  gl.vertexAttribPointer(currentProgram.vertexColorAttribute, this.colorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  for (var i = 0; i < 8; i++) {
+    cycleColors('white', 'black');
+    for (var j = 0; j < 8; j++) {
+      cycleColors('white', 'black');
+      this.createSquare(i, j, color);
+    }
+  }
+}
 
-  if (ChessPlayer.properties.scene.wireframe)
-    gl.drawArrays(gl.LINE_LOOP, 0, this.vertexBuffer.numItems);
-  else
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertexBuffer.numItems);
-};
+Board.prototype.createSquare = function(posX, posZ, color) {
+  this.vertices.push(
+    vec3.fromValues( 0 + posX, 0,  1 + posZ), // a
+    vec3.fromValues( 0 + posX, 0,  0 + posZ), // b
+    vec3.fromValues( 1 + posX, 0,  1 + posZ), // c
+    vec3.fromValues( 1 + posX, 0,  0 + posZ)  // d
+  );
 
-Board.prototype.getVertices = function() {
-  return [
-    vec2.fromValues(-1, 1), // a
-    vec2.fromValues(-1, -1), // b
-    vec2.fromValues(1, 1), // c
-    vec2.fromValues(1, -1)  // d
-  ];
-};
+  var l = this.vertices.length - 4;
+  this.indices.push(
+    l + 0, l + 1, l + 2,
+    l + 1, l + 3, l + 2
+  );
 
-Board.prototype.getIndices = function() {
-  return [
-    0, 1, 2,
-    1, 3, 2
-  ];
-};
+  squareColor = (color == 'black') ? vec4.fromValues(0, 0, 0, 1) : vec4.fromValues(1, 1, 1, 1);
+  this.colors.push(squareColor, squareColor, squareColor, squareColor);
+
+  var up = vec3.fromValues(0, 1, 0);
+  this.normals.push(
+    up, up, up, up
+  );
+}
